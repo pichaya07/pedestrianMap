dab1e3944a5087dd1473bb102997ad7ee18ab2aa
pich_chaya <pichpichaya7@gmail.com>
2016-07-20 16:17:05 +0900

    add routing function
    - routing function using Skobbler service

--- map.js
+++ map.js
@@ -2,14 +2,19 @@
 //map.addLayer(new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'));	//base layer
 //map.locate({setView: true, maxZoom: 16});
 
-var osm_default = new L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+var API_KEY = 'b53b3acef8808a2c0e5fea3c69912e4b';
+
+var osm_default = new L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?highway=path');
 var osm_stamen = new L.tileLayer('http://a.tile.stamen.com/toner/{z}/{x}/{y}.png');
 var osm_transport = new L.tileLayer('http://b.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png');
+var osm_skobbler = new L.tileLayer('http://tiles2-b53b3acef8808a2c0e5fea3c69912e4b.skobblermaps.com/TileService/tiles/2.0/010011101/0/{z}/{x}/{y}.png');
 
-var circle_layer = L.layerGroup();;
+var circle_layer = L.layerGroup();
+var route_layer = L.layerGroup();
+
 
 var map = L.map('map', {
-    layers: [osm_default,circle_layer],
+    layers: [osm_default,circle_layer,route_layer],
     center: [14.069282, 100.606407], //SIIT :) ,
     zoom: 12
 }),drawnItems = L.featureGroup().addTo(map);
@@ -17,11 +22,13 @@ var map = L.map('map', {
 var baseMaps = {
     "Default": osm_default,
     "Stamen": osm_stamen,
-    "Transport" :osm_transport
+    "Transport" :osm_transport,
+    "Skobbler" : osm_skobbler
 };
 
 var overlayMaps = {
-    "Position": circle_layer
+    "Position": circle_layer,
+    "ROUTE" : route_layer
 };
 
 L.control.layers(baseMaps, overlayMaps).addTo(map);
@@ -47,7 +54,8 @@ function onLocationError(e) {
 map.on('locationerror', onLocationError);
 
 
-
+//////  ROUTING  /////////
+//http://b53b3acef8808a2c0e5fea3c69912e4b.tor.skobbler.net/tor/RSngx/calcroute/json/20_5/en/b53b3acef8808a2c0e5fea3c69912e4b?start=46.5472,24.5615&dest=46.5447,24.5607&profile=pedestrian&advice=yes
 
 
 
